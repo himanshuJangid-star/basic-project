046015ca4160104005fd77e2573eedae3464daa5
himanshuJangid-star <himanshujangid257@gmail.com>
2025-08-27 10:54:15 +0530

    Add files via upload

---
+++ prac.c
@@ -0,0 +1,11 @@
+# include <stdio.h> 
+ int main() 
+ {
+   int side;
+   int area;
+   printf("enter the side : ") ;
+   scanf( "%d" , &side);
+   area = side*side ;
+   printf("area is %d" , area) ;
+   return 0 ;
+ }
