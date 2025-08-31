89e5dcb108b1473996f327980276ea210f59c0e9
himanshuJangid-star <himanshujangid257@gmail.com>
2025-08-31 17:26:10 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,14 @@
+//  finding area of circle
+#include <stdio.h>
+float main()
+{
+float pi = 3.14 ;
+float area = 0 ;
+float r = 0 ;
+printf("enter the r");
+scanf("%f", &r);
+area = pi*r*r ;
+
+printf("area is : %f " , area);
+return 0 ;
+}
---
+++ cube.c
@@ -0,0 +1,11 @@
+//finding cube of any integer
+# include <stdio.h>
+int main()
+{
+    int a ;
+printf("enter the value of a = ");
+scanf("%d" ,& a);
+
+printf("cube of no is = %d" , a*a*a);
+return 0 ;
+}
